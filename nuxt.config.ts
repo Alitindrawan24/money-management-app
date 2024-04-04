f65a5c5fb34e817302239cfdb54e4242e8f19e67
Alit Indrawan <alitindrawan71@gmail.com>
2024-04-04 22:42:49 +0800

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  devServer: {
-    port: 3001
-  },
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' }
-  },
-  css: [
-    '~/assets/css/custom.css',
-    '~/assets/css/satoshi.css',
-    '~/assets/css/style.css',
-  ]
+    ssr: false,
+    devtools: { enabled: false },
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    devServer: {
+        port: 3001
+    },
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+        layoutTransition: { name: 'layout', mode: 'out-in' }
+    },
+    css: [
+        '~/assets/css/custom.css',
+        '~/assets/css/satoshi.css',
+        '~/assets/css/style.css',
+    ]
 })
